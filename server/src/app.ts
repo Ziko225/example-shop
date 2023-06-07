6a2d47bab1403de07566de0b0fea8ce50fcde25a
Nazar <nazar@bychyk.it>
2023-06-07 19:28:53 +0200

    Server - Created db model

--- server/src/app.ts
+++ server/src/app.ts
@@ -1,5 +1,7 @@
 import { config } from "dotenv";
+import { sequelize } from "./db.js";
 import express from 'express';
+import "./models/models.js";
 
 config();
 
@@ -7,13 +9,16 @@ const PORT = process.env.PORT || 5500;
 
 const app = express();
 
-const startApp = () => {
+const startApp = async () => {
     try {
+        await sequelize.authenticate();
+        await sequelize.sync();
         app.listen(PORT, () => console.log(`server was started at ${PORT} port `));
 
     } catch (error) {
         console.log(error);
     }
 };
+
 
 startApp();
