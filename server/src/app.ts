8ccd5c22c3ce1becd518ebfb424070846806a7c8
Nazar <nazar@bychyk.it>
2023-06-09 17:19:31 +0200

    Created error handler middleware

--- server/src/app.ts
+++ server/src/app.ts
@@ -4,6 +4,7 @@ import express from 'express';
 import cors from "cors";
 import router from "./routes/index.js";
 import "./models/models.js";
+import { errorHandler } from "./middleware/errorHandler.js";
 
 config();
 
@@ -14,6 +15,8 @@ app.use(cors());
 app.use(express.json());
 
 app.use("/api", router);
+
+app.use(errorHandler);
 
 const startApp = async () => {
     try {
