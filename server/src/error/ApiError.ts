024ed06279349b962a7cfe50d9e0498d677c012a
Nazar <nazar@bychyk.it>
2023-06-10 17:38:26 +0200

    Added initial error names to ApiError

--- server/src/error/ApiError.ts
+++ server/src/error/ApiError.ts
@@ -8,15 +8,23 @@ class ApiError extends Error {
         this.message = message;
     }
 
-    static forbidden(message: string) {
+    static badRequest(message = "The server cannot process the request due to something that is perceived to be a client error / invalid validation") {
+        return new ApiError(400, message);
+    }
+
+    static Unauthorized(message = "Unauthorized. Please login first") {
+        return new ApiError(401, message);
+    }
+
+    static forbidden(message = "Access denied") {
         return new ApiError(403, message);
     }
 
-    static badRequest(message: string) {
+    static notFound(message = "The server cannot find the requested resource") {
         return new ApiError(404, message);
     }
 
-    static internal(message: string) {
+    static internal(message = "The server has encountered a situation it does not know how to handle.") {
         return new ApiError(500, message);
     }
 }
