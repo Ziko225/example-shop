86dd7182eda86d6e88b68cb8ce174c13e9797d93
Nazar <nazar@bychyk.it>
2023-06-10 17:52:51 +0200

    Added logic to typeController and brandController

--- server/src/controllers/brandController.ts
+++ server/src/controllers/brandController.ts
@@ -1,16 +1,53 @@
-import { Request, Response } from "express";
+import { NextFunction, Request, Response } from "express";
+import ApiError from "../error/ApiError.js";
+import models from "../models/models.js";
+
+const { Brand } = models;
 
 class BrandController {
-    async create(req: Request, res: Response) {
-        // code
+    async create(req: Request, res: Response, next: NextFunction) {
+        try {
+            const { name } = req.body;
+            if (!name) {
+                return next(ApiError.badRequest("Name required"));
+            }
+
+            const target = await Brand.findOne({ where: { name } });
+            if (target) {
+                return next(ApiError.badRequest("This name already exists"));
+            }
+
+            const brand = await Brand.create({ name });
+            return res.json(brand);
+        } catch (error) {
+            if (error instanceof Error) {
+                return next(ApiError.internal(error.message));
+            }
+        }
     }
 
-    async getAll(req: Request, res: Response) {
-        // code
+    async getAll(req: Request, res: Response, next: NextFunction) {
+        const brands = await Brand.findAll();
+        if (!brands?.length) {
+            return next(ApiError.notFound("Brands not found"));
+        }
+
+        return res.json(brands);
     }
 
-    async remove(req: Request, res: Response) {
-        // code
+    async remove(req: Request, res: Response, next: NextFunction) {
+        const { id } = req.params;
+        if (!id) {
+            return next(ApiError.badRequest("Id required"));
+        }
+
+        const brand = await Brand.findOne({ where: { id } });
+        brand && await brand.destroy();
+
+        if (brand) {
+            return res.status(200).json(brand);
+        }
+        return next(ApiError.notFound());
     }
 }
 
--- server/src/controllers/typeController.ts
+++ server/src/controllers/typeController.ts
@@ -1,16 +1,53 @@
-import { Request, Response } from "express";
+import { NextFunction, Request, Response } from "express";
+import models from "../models/models.js";
+import ApiError from "../error/ApiError.js";
+
+const { Type } = models;
 
 class TypeController {
-    async create(req: Request, res: Response) {
-        // code
+    async create(req: Request, res: Response, next: NextFunction) {
+        try {
+            const { name } = req.body;
+            if (!name) {
+                return next(ApiError.badRequest("Name required"));
+            }
+
+            const target = await Type.findOne({ where: { name } });
+            if (target) {
+                return next(ApiError.badRequest("This name already exists"));
+            }
+
+            const type = await Type.create({ name });
+            return res.json(type);
+        } catch (error) {
+            if (error instanceof Error) {
+                return next(ApiError.internal(error.message));
+            }
+        }
     }
 
-    async getAll(req: Request, res: Response) {
-        // code
+    async getAll(req: Request, res: Response, next: NextFunction) {
+        const types = await Type.findAll();
+        if (!types?.length) {
+            return next(ApiError.notFound("Types not found"));
+        }
+
+        return res.json(types);
     }
 
-    async remove(req: Request, res: Response) {
-        // code
+    async remove(req: Request, res: Response, next: NextFunction) {
+        const { id } = req.params;
+        if (!id) {
+            return next(ApiError.badRequest("Id required"));
+        }
+
+        const type = await Type.findOne({ where: { id } });
+        type && await type.destroy();
+        if (type) {
+            return res.status(200).json(type);
+        }
+
+        return next(ApiError.notFound());
     }
 }
 
